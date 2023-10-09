5d526c0d4dd62d40795f78c553b02c306d8fb771
Alyona1619 <108504553+Alyona1619@users.noreply.github.com>
2023-10-10 00:17:28 +0800

    Update prefix_to_infix.py

--- prefix_to_infix/src/prefix_to_infix.py
+++ prefix_to_infix/src/prefix_to_infix.py
@@ -9,12 +9,14 @@ def to_infix(expression):
         if operator.isdigit():
             stack.append(operator)
         elif is_operator(operator):
+            if len(stack) < 2:
+                raise ValueError("Операторов больше, чем нужно для операндов")
             operand1 = stack.pop()
             operand2 = stack.pop()
             infix_expression = f"({operand1} {operator} {operand2})"
             stack.append(infix_expression)
         else:
-            raise ValueError(f"Неизвестный оператор: {operator}")
+            raise ValueError(f"Неизвестный оператор или операнд: {operator}")
 
     if len(stack) != 1:
         raise ValueError("Некорректное выражение")
