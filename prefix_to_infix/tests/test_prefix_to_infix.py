ce5a684b405b685275b2337cac9590cb78c25c84
Alyona1619 <108504553+Alyona1619@users.noreply.github.com>
2023-10-09 23:57:43 +0800

    Update test_prefix_to_infix.py

--- prefix_to_infix/tests/test_prefix_to_infix.py
+++ prefix_to_infix/tests/test_prefix_to_infix.py
@@ -7,37 +7,46 @@ def operators():
     return '+', '-', '*', '/'
 
 
-def test_valid_prefix_expression():
-    expression = "+ - 13 4 55"
-    result = to_infix(expression)
-    assert result == "((13 - 4) + 55)"
+@pytest.fixture
+def valid_expressions():
+    return (
+        ('+ - 13 4 55', '((13 - 4) + 55)'),
+        ('+ 2 * 2 - 2 1', '(2 + (2 * (2 - 1)))'),
+        ('/ + 3 10 * + 2 3 - 3 5', '((3 + 10) / ((2 + 3) * (3 - 5)))'),
+        ('+ + 10 20 30', '((10 + 20) + 30)'),
+    )
 
 
-def test_invalid_prefix_expression():
-    expression = "+ - 13 4 55 10"
-    with pytest.raises(ValueError) as e_info:
-        to_infix(expression)
+class TestPrToIn:
 
+    def test_valid_prefix_expression(self, valid_expressions):
+        for prefix_expression, expected_infix_expression in valid_expressions:
+            result = to_infix(prefix_expression)
+            assert result == expected_infix_expression
 
-def test_empty_prefix_expression():
-    expression = ""
-    with pytest.raises(ValueError) as e_info:
-        to_infix(expression)
+    def test_invalid_prefix_expression(self):
+        expression = "+ - 13 4 55 10"
+        with pytest.raises(ValueError) as e_info:
+            to_infix(expression)
 
+    def test_empty_prefix_expression(self):
+        expression = ""
+        with pytest.raises(ValueError) as e_info:
+            to_infix(expression)
 
-def test_operator_precedence():
-    expression = "+ * 2 3 - 5 4"
-    result = to_infix(expression)
-    assert result == "((2 * 3) + (5 - 4))"
+    # def test_operator_priority(self):
+    #     expression = "+ * 2 3 - 5 4"
+    #     result = to_infix(expression)
+    #     assert result == "((2 * 3) + (5 - 4))"
 
+    def test_is_operator(self, operators):
+        op1, op2, op3, op4 = operators
+        res = is_operator(op1)
+        res1 = is_operator(op2)
+        res2 = is_operator(op3)
+        res3 = is_operator(op4)
+        assert res == res1 == res2 == res3 is True
 
-def test_is_operator():
-    oper = '/'
-    result = is_operator(oper)
-    assert result == True
-
-
-def test_is_operator1():
-    oper = '+'
-    result = is_operator(oper)
-    assert result == True
+    def test_invalid_operator(self):
+        with pytest.raises(ValueError, match=f"Неизвестный оператор: @"):
+            to_infix("@ 1 2")
